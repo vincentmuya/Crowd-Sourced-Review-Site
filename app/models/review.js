445fdc12e07da230b6fa5e02baac3aaa7e282b5b
vincentmuya <vincentmuya13@gmail.com>
2017-11-05 18:11:26 +0300

    added review model

--- app/models/review.js
+++ app/models/review.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  rating: DS.attr(),
+  comment: DS.attr(),
+  author: DS.attr(),
+  restaurant: DS.belongsTo("restaurant", {async:true}),
 });
